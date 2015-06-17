291e84c51201de78f465d657d9795c346f572f69
AgileLabs <samer@agilelabs.com>
2015-06-17 15:35:35 -0700

    Prep for players

--- js/main.js
+++ js/main.js
@@ -1,29 +1,41 @@
-var myFirebaseRef = new Firebase("https://keep-stuff.firebaseio.com/");
-
+var ref = new Firebase("https://cht3game.firebaseio.com");
+var gameRef = ref.child("game");
 
 $(document).ready(function(){
-
   $("button#Login").on("click", function(){
-    myFirebaseRef.authWithOAuthPopup("twitter", function(error, authData) {
+    ref.authWithOAuthPopup("twitter", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
       } else {
         console.log("Authenticated successfully with payload:", authData);
-        $(".hidden").removeClass("hidden");
       }
     });
   });
 });
 
+var Game = {};
+
+gameRef.on("value", function(snap) {
+  var game = snap.val();
+  Game.players = game.players;
+
+  $("#first-player").text(Game.players.player1);
+  $("#second-player").text(Game.players.player2);
+});
+
 var isNewUser = true;
-myFirebaseRef.onAuth(function(authData) {
+ref.onAuth(function(authData) {
   if (authData && isNewUser) {
     // save the user's profile into Firebase so we can list users,
     // use them in Security and Firebase Rules, and show profiles
-    myFirebaseRef.child("users").child(authData.uid).set({//nested children of root -- this is kind of schema setup.
-      provider: authData.provider,
-      name: getName(authData)
-    });
+    // ref.child("users").child(authData.uid).set({//nested children of root -- this is kind of schema setup.
+    //   provider: authData.provider,
+    //   name: getName(authData)
+    // });
+    $(".hidden").removeClass("hidden");
+    var currentUsername = authData.twitter.username;
+    // if you're not a player already, and we're still accepting players (players < 2)
+    // gameRef.child("players").set({ player1: currentUserName });
   }
 });
 
