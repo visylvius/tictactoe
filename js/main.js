d36a6fb27e9b2a6b0b3d44f7b72c84b6b49d4833
AgileLabs <samer@agilelabs.com>
2015-06-17 19:23:40 -0700

    Clicking X and O, fixed the login

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var ref = new Firebase("https://cht3game.firebaseio.com");
+var ref = new Firebase("https://cht3game.firebaseio.com/adsjfasdjfsadjfklasdfjlsdakfjalskdjfkldsaflk");
 var gameRef = ref.child("game");
 
 $(document).ready(function(){
@@ -11,12 +11,34 @@ $(document).ready(function(){
       }
     });
   });
+
+  $(".cell").on("click", function() {
+    var currentMark = Game.currentMark();
+    if (currentMark) {
+      $(this).addClass(currentMark).text(currentMark);
+    }
+  });
 });
 
 var Game = {};
 
-gameRef.on("value", function(snap) {
+Game.currentMark = function() {
+  if (Game.x === Game.currentUsername) {
+    return 'x';
+  }
+  if (Game.o === Game.currentUsername) {
+    return 'o';
+  }
+  return null;
+}
+
+// { gc1: 'x', gc2: 'o' ... }
+
+gameRef.on("value", assignPlayers);
+
+function assignPlayers(snap) {
   var game = snap.val();
+  console.log("Snap Val:", game);
   if (!game) {
     return;
   }
@@ -26,35 +48,40 @@ gameRef.on("value", function(snap) {
 
   $("#first-player").text(Game.players.x + " - X");
   $("#second-player").text(Game.players.o + " - O");
+}
 
-  Game.nextPlayer = function() {
-    if (!this.x) {
-      return 'x';
-    }
-    if (!this.o) {
-      return 'o';
-    }
-    return null;
+Game.nextPlayer = function() {
+  console.log(this);
+  if (!this.x) {
+    return 'x';
   }
+  if (!this.o) {
+    return 'o';
+  }
+  return null;
+}
 
-  var isNewUser = true;
-  ref.onAuth(function(authData) {
-    if (authData && isNewUser) {
-      // save the user's profile into Firebase so we can list users,
-      // use them in Security and Firebase Rules, and show profiles
-      // ref.child("users").child(authData.uid).set({//nested children of root -- this is kind of schema setup.
-      //   provider: authData.provider,
-      //   name: getName(authData)
-      // });
-      $(".hidden").removeClass("hidden");
+var isNewUser = true;
+ref.onAuth(function(authData) {
+  console.log("Auth:", authData);
+  if (authData && isNewUser) {
+    // save the user's profile into Firebase so we can list users,
+    // use them in Security and Firebase Rules, and show profiles
+    // ref.child("users").child(authData.uid).set({//nested children of root -- this is kind of schema setup.
+    //   provider: authData.provider,
+    //   name: getName(authData)
+    // });
+    gameRef.once("value", function(snap) {
+      assignPlayers(snap);
       Game.currentUsername = authData.twitter.username;
       var options = {}, nextPlayer = Game.nextPlayer();
+      console.log(nextPlayer);
       if (nextPlayer) {
         options[nextPlayer] = Game.currentUsername;
         gameRef.child("players").update(options);
       }
-    }
-  });
+    });
+  }
 });
 
 function getName(authData) {
