b056ed45cecce6c77afe2502d2776de86b101a70
AgileLabs <samer@agilelabs.com>
2015-06-18 17:28:56 -0700

    Refactor

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var FB = {};
 
 FB.ref = new Firebase("https://cht3game.firebaseio.com/sdfjkadskljfloeqworuoierutoie");
 FB.gameRef = FB.ref.child("game");
+FB.playersRef = FB.gameRef.child("players");
 FB.gridRef = FB.gameRef.child("grid");
 
 $(document).ready(function(){
@@ -22,7 +23,7 @@ $(document).ready(function(){
     if (currentMark) {
       clickedCell = $(this).data("cell");
       gridUpdate = {};
-      gridUpdate["gc-" + clickedCell] = currentMark;
+      gridUpdate[clickedCell] = currentMark;
       FB.gridRef.update(gridUpdate);
       $(this).addClass(currentMark).text(currentMark);
     }
@@ -41,22 +42,24 @@ Game.currentMark = function() {
   return null;
 }
 
-// { gc1: 'x', gc2: 'o' ... }
-
-FB.gameRef.on("value", assignPlayers);
+FB.playersRef.on("value", assignPlayers);
+FB.gridRef.on("value", redrawGrid);
 
 function assignPlayers(snap) {
-  var game = snap.val();
-  console.log("Snap Val:", game);
-  if (!game) {
+  var players = snap.val();
+  console.log("Snap Val:", players);
+  if (!players) {
     return;
   }
-  Game.players = game.players;
-  Game.x = game.players.x;
-  Game.o = game.players.o;
+  Game.players = players;
+  Game.x = players.x;
+  Game.o = players.o;
 
   $("#first-player").text(Game.players.x + " - X");
   $("#second-player").text(Game.players.o + " - O");
+}
+function redrawGrid(snap) {
+  console.log(snap.val());
 }
 
 Game.nextPlayer = function() {
@@ -80,14 +83,14 @@ FB.ref.onAuth(function(authData) {
     //   provider: authData.provider,
     //   name: getName(authData)
     // });
-    FB.gameRef.once("value", function(snap) {
+    FB.playersRef.once("value", function(snap) {
       assignPlayers(snap);
       Game.currentUsername = authData.twitter.username;
       var options = {}, nextPlayer = Game.nextPlayer();
       console.log(nextPlayer);
       if (nextPlayer) {
         options[nextPlayer] = Game.currentUsername;
-        FB.gameRef.child("players").update(options);
+        FB.playersRef.update(options);
       }
     });
   }
